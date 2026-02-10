61e3ef76c2e76e41c655bc0c921c9049c18e5616
Qluwerty <laskowskiszymon53@gmail.com>
2026-02-11 00:36:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -36,6 +36,19 @@ noBtn.addEventListener("mouseover", () => {
     noBtn.style.transform = `translate(${moveX}px, ${moveY}px)`;
 });
 
+noBtn.addEventListener("touchstart", () => {
+    const min = 200;
+    const max = 200;
+
+    const distance = Math.random() * (max - min) + min;
+    const angle = Math.random() * Math.PI * 2;
+
+    const moveX = Math.cos(angle) * distance;
+    const moveY = Math.sin(angle) * distance;
+
+    noBtn.style.transition = "transform 0.3s ease";
+    noBtn.style.transform = `translate(${moveX}px, ${moveY}px)`;
+});
 // Logic to make YES btn to grow
 
 // let yesScale = 1;
@@ -70,3 +83,4 @@ yesBtn.addEventListener("click", () => {
 
     finalText.style.display = "block";
 });
+
